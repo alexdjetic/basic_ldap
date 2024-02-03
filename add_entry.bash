e3cba367ff2e5fe68f67cbbf093dc7ad0370e589
alexandredjetic <121016019+alexdjetic@users.noreply.github.com>
2024-02-03 17:02:58 +0100

    Add files via upload

--- add_entry.bash
+++ add_entry.bash
@@ -5,13 +5,18 @@
 # Description: this script add an entry to ldap server
 
 if [ $# -ne 1 ]; then
-	echo "Usage: $0 <file.ldif>"
+	echo "Usage: $0 <file.ldif> <password>"
 	exit 1
 fi
 
 if [ -e "$1" ]; then
+  if [ $# -eq 2 ]; then
+    ldapadd -x -D "cn=admin,dc=solea,dc=local" -w "$2" -H ldap:/// -f "$1"
+  else
+    ldapadd -x -D "cn=admin,dc=solea,dc=local" -W -H ldap:/// -f "$1"
+  fi
+
 	ldapadd -x -D "cn=admin,dc=solea,dc=local" -W -H ldap:/// -f $1
-	exit 0
 else
 	echo "file do not exist...exiting"
 	exit 1
--- setup_ldap.bash
+++ setup_ldap.bash
@@ -4,12 +4,36 @@
 # Modified: 04/02/2024
 # Description: this script setup an LDAP server on ubuntu server
 
+#install package
 sudo apt install git neovim vim nano curl -y
 sudo apt install slapd ldap-utils -y
 sudo dpkg-reconfigure slapd
 ldapsearch -Q -LLL -Y EXTERNAL -H ldapi:/// -b cn=config dn
 ldapsearch -x -LLL -H ldap:/// -b dc=solea,dc=local
-sudo systemctl start slapd
-sudo systemctl status slapd
 
+#setup firewall
 sudo ./setup_firewall.bash
+
+#admin password
+echo "give admin password: "
+read pass
+
+# add all entry to DLAP server
+## OU
+ou_order=$(cat list_import_ou.txt)
+
+while IFS= read -r ou; do
+  ./add_entry.bash "ou/$ou.ldif" "$pass"
+done < "$ou_order"
+
+## user
+for user in $(ls user/*.ldif)
+do
+  ./add_entry.bash $user "$pass"
+done
+
+## group
+for group in $(ls group/*.ldif)
+do
+  ./add_entry.bash $group "$pass"
+done
